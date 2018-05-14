b77a8b5092cf9d97b5a5907283768759107d35d6
umutserifler@gmail.com <umutserifler@hotmail.com>
2018-05-14 11:10:25 +0200

    Initial Commit

---
+++ CoreLocationSwift/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  CoreLocationSwift
+//
+//  Created by UmutSERIFLER on 14/05/2018.
+//  Copyright © 2018 UmutSERIFLER. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("Location : \(locValue.latitude) \(locValue.longitude)")
+        
+        locationManager.stopUpdatingLocation()
+    }
+
+    @IBAction func getCurrentLocation(_ sender: Any) {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+        
+    }
+    
+}
+
